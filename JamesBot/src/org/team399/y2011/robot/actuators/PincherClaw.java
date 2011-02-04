1869d1fc2116b8e4cf60093e7a57f104ac0d613d
jeremy.germita <jeremy.germita@gmail.com>
2011-02-04 00:40:33 +0000

    Added EasyButton Class Jackie wrote pincher claw code

--- JamesBot/src/org/team399/y2011/robot/actuators/PincherClaw.java
+++ JamesBot/src/org/team399/y2011/robot/actuators/PincherClaw.java
@@ -4,11 +4,23 @@
  */
 
 package org.team399.y2011.robot.actuators;
-
+import edu.wpi.first.wpilibj.Solenoid;
 /**
  *
  * @author Jeremy Germita, Jackie Patton
  */
 public class PincherClaw {
+    
+    private Solenoid ClawA = new Solenoid(1);   //Solenoid on port 1
+    private Solenoid ClawB = new Solenoid(2);   //Solenoid on port 2
+
+    public PincherClaw() {
+
+    }
+
+    public void grab(boolean state) {
+        ClawA.set(state);               //sets to boolean state
+        ClawB.set(!state);              //sets opposite boolean state
+    }
 
 }
