15781a6b08985ca13ddfab3b00ccf14679c35ec1
jeremy.germita <jeremy.germita@gmail.com>
2011-02-12 20:15:49 +0000

    Added some functions to the driver station user interface. Modified Teleop code for operator control Added to the Integral gain in the arm code Camera pan added by john

--- JamesBot/src/org/team399/y2011/HumanInterfaceDevices/DriverStationUserInterface.java
+++ JamesBot/src/org/team399/y2011/HumanInterfaceDevices/DriverStationUserInterface.java
@@ -15,25 +15,98 @@ public class DriverStationUserInterface {
 
     private DriverStationEnhancedIO m_io;   //IO board object
 
-    final short BLUE_BUTTON   = 1;
-    final short RED_BUTTON    = 2;
-    final short WHITE_BUTTON  = 3;
-    final short BLACK_BUTTON  = 4;
-    final short TOGGLE_SWITCH = 5;
-    final short MISSLE_SWITCH = 6;
-    final short EASY_BUTTON   = 7;
+    final short BLUE_BUTTON     = 1;
+    final short RED_BUTTON      = 2;
+    final short WHITE_BUTTON    = 3;
+    final short BLACK_BUTTON    = 4;
+    final short TOGGLE_SWITCH   = 5;
+    final short MISSILE_SWITCH  = 6;
+    final short EASY_BUTTON     = 7;
 
 
+    /**
+     * Constructor.
+     */
     public DriverStationUserInterface() {
         m_io = DriverStation.getInstance().getEnhancedIO(); //Instantiating the object
     }
 
-    public boolean getBlueSwitch() {
+    /**
+     * Get the state of the blue button
+     * @return blue button's state
+     */
+    public boolean getBlueButton() {
+        return getDigital(BLUE_BUTTON);
+    }
+
+    /**
+     * Get the state of the red button
+     * @return The red button's state
+     */
+    public boolean getRedButton() {
+        return getDigital(RED_BUTTON);
+    }
+
+    /**
+     * Get the state of the white button
+     * @return The white button's state
+     */
+    public boolean getWhiteButton() {
+        return getDigital(WHITE_BUTTON);
+    }
+
+    /**
+     * Get the state of the black button
+     * @return The black button's state
+     */
+    public boolean getBlackButton() {
+        return getDigital(BLACK_BUTTON);
+    }
+
+    /**
+     * Get the state of the toggle switch
+     * @return The toggle switch's state
+     */
+    public boolean getToggleSwitch() {
+        return getDigital(TOGGLE_SWITCH);
+    }
+
+    /**
+     * Get the state of the missile switch
+     * @return The missle switch's state
+     */
+    public boolean getMissileSwitch() {
+        return getDigital(MISSILE_SWITCH);
+    }
+
+    /**
+     * Get the state of the easy button
+     * @return <b><i> that was easy </i></b>(The state of the easy button)
+     */
+    public boolean getEasyButton() {
+        return getDigital(EASY_BUTTON);
+    }
+
+    /**
+     * Get the state of a digital input
+     * @param which The channel
+     * @return The state of the digital input
+     */
+    public boolean getDigital(int which) {
         try {
-            return m_io.getDigital(BLUE_BUTTON);
+            return m_io.getDigital(which);
         } catch(Exception e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public double getAnalog(int which) {
+        try {
+            return m_io.getAnalogIn(which);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return 0.0;
         }
     }
 }
--- JamesBot/src/org/team399/y2011/robot/JamesBot.java
+++ JamesBot/src/org/team399/y2011/robot/JamesBot.java
@@ -14,6 +14,7 @@ import org.team399.y2011.robot.autonomous.LineFollow;
 import org.team399.y2011.robot.autonomous.AutonomousRoutines;
 import org.team399.y2011.HumanInterfaceDevices.Attack3Joystick;
 import org.team399.y2011.HumanInterfaceDevices.Rumblepad2GamePad;
+import org.team399.y2011.HumanInterfaceDevices.DriverStationUserInterface;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Compressor;
 
@@ -32,9 +33,13 @@ public class JamesBot extends IterativeRobot {
     Attack3Joystick leftJoy  = new Attack3Joystick(1);    //Left Joystick
     Attack3Joystick rightJoy = new Attack3Joystick(2);    //Right Joystick
     Rumblepad2GamePad operator = new Rumblepad2GamePad(3);//Operator gamepad
+    DriverStationUserInterface io = new DriverStationUserInterface();
 
     Arm arm               = new Arm();              //Arm instance
     Compressor compressor = new Compressor(14,1);   //Compressor instance
+
+    int autonomousMode = 0;
+    String autonomousSide = "None";
     
     public void disabledInit() {
         arm.disable();  //Disable arm PID control
@@ -42,27 +47,48 @@ public class JamesBot extends IterativeRobot {
 
     public void teleopInit() {
         compressor.start(); //Start compressor
-        arm.enable();   //Enable arm PID
+        arm.enable();       //Enable arm PID
+        arm.setPoint(1.624);//Fold the arm in
     }
 
     public void disabledPeriodic() {
         arm.print();    //Print arm pot value
+        if(io.getToggleSwitch()) {
+            if(io.getBlueButton()) {
+                autonomousMode = 1;
+            }
+            if(io.getRedButton()) {
+                autonomousMode = 2;
+            }
+            if(io.getWhiteButton()) {
+                autonomousSide = "Left";
+            }
+            if(io.getBlackButton()) {
+                autonomousSide = "Right";
+            }
+        } else {
+            autonomousMode = 0;
+        }
     }
 
     public void teleopPeriodic() {
         robot.tankDrive(leftJoy.getY(),
                         -rightJoy.getY());   //Tank drive, two sticks
-        arm.print();    //Print pot value
-        
-        arm.setPoint(arm.getSetpoint() +
-                -operator.getRightY()*.3); //Arm angle control, operator right stick
+        arm.setPoint(arm.getSetpoint() + (-(operator.getRightY())*.05));
 
-        if(operator.getButton(11)) {
-            compressor.stop();
-        } else if(operator.getButton(5)){
-            compressor.start();
+        if(operator.getButton(2)) {             //If the operator presses button 2,
+            arm.setPoint(Arm.ArmStates.LOW);    //Bring the arm into the low position
+        } else if(operator.getButton(3)) {      //Else if they press button 3,
+            arm.setPoint(Arm.ArmStates.MID);    //Bring the arm into the mid position
+        } else if(operator.getButton(4)) {      //else if they press button 4,
+            arm.setPoint(Arm.ArmStates.HIGH);   //Bring the arm into the high posision
+        } else if(operator.getButton(1)) {      //Else if they press button 1,
+            arm.setPoint(Arm.ArmStates.GROUND); //Bring it into the ground position
+        } else if(operator.getButton(6)) {      //Else if they press button ,
+            arm.setPoint(Arm.ArmStates.INSIDE); //Stow the arm
         }
-
+        
+        
     }
 
     public void teleopContinuous() {
@@ -70,11 +96,16 @@ public class JamesBot extends IterativeRobot {
     }
 
     public void autonomousInit() {
-        
+        arm.enable();
+        AutonomousRoutines.setSide(autonomousSide);
     }
-    public void autonomousPeriodic() {
-        AutonomousRoutines.disabled();
+    public void autonomousContinuous() {
+        switch(autonomousMode) {
+            case 0: AutonomousRoutines.disabled(); break;
+            case 1: AutonomousRoutines.autonOne(); break;
+            case 2: AutonomousRoutines.autonTwo(); break;
+        }
     }
 
     
-}
+    }
